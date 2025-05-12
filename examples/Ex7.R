826642be5a6a796b5a280c97039b0f7c33148143
antoniocarlosrodriguezbajo@gmail.com <antoniocarlosrodriguezbajo@gmail.comm>
2025-05-12 22:39:46 +0200

    Ex7: Example of integration with MATLAB and unsupervised feature selection.

--- examples/Ex7.R
+++ examples/Ex7.R
@@ -1,21 +1,33 @@
 # Connection to MATLAB
 
 library(R.matlab)
+library(diceRplus)
+library(RPEClust)
+
 Matlab$startServer()
-
 matlab <- Matlab()
-
 print(matlab)
 
 isOpen <- open(matlab)
 if (!isOpen)
-  throw("MATLAB server is not running: waited 30 seconds.")
+  print("MATLAB server is not running: waited 30 seconds.")
 print(matlab)
 
-# Carga el archivo COIL20.mat
-evaluate(matlab, "load('COIL20.mat');")
-# Ejecuta Auto_UFSTool
-evaluate(matlab,"Result = Auto_UFSTool(X, 'RUFS');")
+data(meats)
+
+str(meats$class)
+
+dat <-  meats[, !names(meats) %in% "class"]
+
+ref.cl <- as.integer(meats$class)
+
+# setVariable(matlab, X = Meat$x)
+setVariable(matlab, X = dat)
+
+evaluate(matlab, "whos")
+
+evaluate(matlab, "rng(42);")
+evaluate(matlab, "Result = Auto_UFSTool(X, 'RUFS');")
 result <- getVariable(matlab, "Result")
 # Extraer el vector numérico
 ranking <- result$Result[[1]]
@@ -27,6 +39,53 @@ head(ranking)
 
 # O imprimir todo el ranking:
 print(ranking)
+
+close(matlab)
+
+
+######
+
+
+
+
+# Carga el archivo COIL20.mat
+evaluate(matlab, "load('COIL20.mat');")
+# Ejecuta Auto_UFSTool
+evaluate(matlab, "rng(42);")
+evaluate(matlab,"Result = Auto_UFSTool(X, 'RUFS');")
+
+
+result <- getVariable(matlab, "Result")
+# Extraer el vector numérico
+ranking <- result$Result[[1]]
+# Aplana el array:
+ranking <- as.vector(ranking)
+
+# Ahora puedes ver los primeros elementos con:
+head(ranking)
+
+# O imprimir todo el ranking:
+print(ranking)
+
+# String to send option 1 to MATLAB Server
+codigo <- paste(
+  "robot = java.awt.Robot;",
+  "pause(4);",
+  "robot.keyPress(java.awt.event.KeyEvent.VK_1);",
+  "robot.keyRelease(java.awt.event.KeyEvent.VK_1);",
+  "robot.keyPress(java.awt.event.KeyEvent.VK_ENTER);",
+  "robot.keyRelease(java.awt.event.KeyEvent.VK_ENTER);",
+  "Result = Auto_UFSTool(X, 'RUFS');",
+  sep = " "
+)
+
+path_MATLAB = "C:/Users/anton/OneDrive/Documentos/MATLAB"
+
+fileConn <- file(paste0(path_MATLAB, "/AutoRun.m"))
+writeLines(codigo, fileConn)
+close(fileConn)
+
+
 
 
 
