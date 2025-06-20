7d041b8f73899dcf5cfd306327987152a846214e
antoniocarlosrodriguezbajo@gmail.com <antoniocarlosrodriguezbajo@gmail.comm>
2025-06-21 00:31:15 +0200

    More experiments in multidataset_experiments.R

--- experiments/multidataset_experiments.R
+++ experiments/multidataset_experiments.R
@@ -25,10 +25,10 @@ calculate_consensus_labels <- function (E, k, ref_labels) {
   )
 
   # Apply each consensus method
-  consensus_labels <- lapply(consensus_methods, function(f) f(E))
+  consensus_labels = lapply(consensus_methods, function(f) f(E))
 
   # Relabel
-  aligned_labels <- list(
+  aligned_labels = list(
     kmodes   = relabel_class(consensus_labels$kmodes, ref_labels),
     majority = relabel_class(consensus_labels$majority, ref_labels),
     lce      = relabel_class(consensus_labels$lce, ref_labels),
@@ -39,9 +39,29 @@ calculate_consensus_labels <- function (E, k, ref_labels) {
   return(aligned_labels)
 }
 
+# Calcute internal_metrics based on consensus_evaluate of diceR
+calculate_internal_metrics <- function(data,cluster_labels) {
+  num_labels <- length(cluster_labels)
+
+  # Create structure for consensus_evaluate
+  cc_data = array(cluster_labels, dim = c(num_labels, 1, 1, 1))
+  row_names = rownames(data)
+  dimnames(cc_data) = list(
+    row_names,  # Primer nivel de nombres: nombres de las filas de Meat$x
+    "R1",       # Repetition
+    "RPGMMClu", # Clustering algorithm
+    "5"         # Number of clusters
+  )
+  # Evaluation
+  result_evaluation = consensus_evaluate(data, cc_data)
+  metrics_df = result_evaluation$ii[[1]]
+  metrics_list = lapply(metrics_df[1, -1], function(x) unname(x))
+  return(metrics_list)
+}
+
 calculate_external_metrics <- function (consensus_labels, ref_labels) {
   # Evaluate results
-  eval_results <- lapply(consensus_labels, function(labels) list(
+  eval_results = lapply(consensus_labels, function(labels) list(
     confmat = ev_confmat(labels, ref_labels),
     nmi     = ev_nmi(labels, ref_labels),
     ari     = adjustedRandIndex(labels, ref_labels)
@@ -50,90 +70,78 @@ calculate_external_metrics <- function (consensus_labels, ref_labels) {
   return(eval_results)
 }
 
-# Calcute internal_metrics based on consensus_evaluate of diceR
-calculate_internal_metrics <- function(data,cluster_labels) {
-  num_labels <- length(cluster_labels)
-
-  # Create structure for consensus_evaluate
-  cc_data <- array(cluster_labels, dim = c(num_labels, 1, 1, 1))
-  row_names <- rownames(data)
-  dimnames(cc_data) <- list(
-    row_names,  # Primer nivel de nombres: nombres de las filas de Meat$x
-    "R1",       # Repetition
-    "RPGMMClu", # Clustering algorithm
-    "5"         # Number of clusters
-  )
-  # Evaluation
-  result_evaluation <- consensus_evaluate(data, cc_data)
-  metrics_df <- result_evaluation$ii[[1]]
-  metrics_list <- lapply(metrics_df[1, -1], function(x) unname(x))
-  return(metrics_list)
-}
-
-
-run_RPClu_experiments <- function(data, ref_labels, algorithms, B, k) {
+run_RPClu_experiments <- function(data_all, top_features, ref_labels, algorithms, B, k) {
+  data <- data_all[,top_features]
+  experiment_ids <- c()
   for (alg in algorithms) {
     print(alg)
     tryCatch({
-      execution_time <- system.time(RPClu_results <- RPClu_parallel(data,
+      execution_time = system.time(RPClu_results <- RPClu_parallel(data,
                                                                     clust_fun = alg,
                                                                     g = k,
                                                                     B = B,
                                                                     verb = TRUE))["elapsed"]
 
-      E <- as.matrix(RPClu_results$clusterings)
+      E = as.matrix(RPClu_results$clusterings)
 
       if (B > 1) {
-        consensus_labels <- calculate_consensus_labels(E, k, ref_labels)
+        consensus_labels = calculate_consensus_labels(E, k, ref_labels)
       } else {
-        consensus_labels <- list(individual = as.vector(E))
+        consensus_labels = list(individual = as.vector(E))
       }
 
-      external_metrics <- calculate_external_metrics(consensus_labels, ref_labels)
+      external_metrics = calculate_external_metrics(consensus_labels, ref_labels)
       for (con_method in names(external_metrics)) {
-        ari_valor <- round(external_metrics[[con_method]]$ari, 4)
-        nmi_valor <- round(external_metrics[[con_method]]$nmi, 4)
-        acc_valor <- external_metrics[[con_method]]$confmat$.estimate[external_metrics[[con_method]]$confmat$.metric == "accuracy"]
-        acc_valor <- round(acc_valor, 4)
+        ari_valor = round(external_metrics[[con_method]]$ari, 4)
+        nmi_valor = round(external_metrics[[con_method]]$nmi, 4)
+        acc_valor = external_metrics[[con_method]]$confmat$.estimate[external_metrics[[con_method]]$confmat$.metric == "accuracy"]
+        acc_valor = round(acc_valor, 4)
         cat(sprintf("%s: ARI:%.4f NMI:%.4f ACC:%.4f \n",
                     con_method, ari_valor, nmi_valor, acc_valor))
 
         # Log the experiment
-        exp_data <- experiment_logger(
-          description = "Clustering with RPGMMClu - Experiment in Anderlucci's paper (parallel implementation)",
+        exp_data = experiment_logger(
+          description = paste0("Ensemble Clustering with Inf-FS2020 + RP +",
+                               alg,"+", con_method),
           dataset = "Meat",
+          clustering_method = alg,
+          clustering_method_params = list(k = k, B = B),
           UFS_method = "Inf-FS2020",
           num_features = ncol(data),
-          ensemble_method = paste0("RP-", alg, "-", con_method),
+          features = top_features,
+          ensemble_method = con_method,
           ensemble_method_params = list(k = k, B = B),
           execution_time = as.numeric(execution_time),
-          labels_clustering = consensus_labels,
-          # internal_metrics = internal_metrics_p,
+          labels_clustering = consensus_labels[[con_method]],
+          # Calculate internal metrics
+          internal_metrics = calculate_internal_metrics(data,consensus_labels[[con_method]]),
+          # Calculate external metrics
           external_metrics = list(ari = ari_valor, nmi = nmi_valor, acc = acc_valor)
         )
-        print(exp_data)
+        save_experiment(exp_data)
+        experiment_ids <- c(experiment_ids, exp_data$experiment_id)
       }
 
     },
     error = function(e) {
       message("Error: ", conditionMessage(e))
-      NA
     })
   }
+  return(c(first = experiment_ids[1], last = tail(experiment_ids, 1)))
 }
 
 run_experiment_individual_algorithm <- function(data, ref_labels, algorithm, k) {
-  clustering_labels <- consensus_cluster(data, nk = k,
+  clustering_labels = consensus_cluster(data, nk = k,
                                              reps = 1, p.item = 1,
                                              algorithms = algorithm, progress = FALSE)
-  clustering_labels <- relabel_class(clustering_labels, ref_labels)
-  clustering_labels <- list(individual = clustering_labels)
-  external_metrics <- calculate_external_metrics(clustering_labels, ref_labels)
+  clustering_labels = relabel_class(clustering_labels, ref_labels)
+  clustering_labels = list(individual = clustering_labels)
+  external_metrics = calculate_external_metrics(clustering_labels, ref_labels)
   con_method = "individual"
-  ari_valor <- round(external_metrics[[con_method]]$ari, 4)
-  nmi_valor <- round(external_metrics[[con_method]]$nmi, 4)
-  acc_valor <- external_metrics[[con_method]]$confmat$.estimate[external_metrics[[con_method]]$confmat$.metric == "accuracy"]
-  acc_valor <- round(acc_valor, 4)
+  ari_valor = round(external_metrics[[con_method]]$ari, 4)
+  nmi_valor = round(external_metrics[[con_method]]$nmi, 4)
+  acc_valor = external_metrics[[con_method]]$confmat$.estimate[external_metrics[[con_method]]$confmat$.metric == "accuracy"]
+  acc_valor = round(acc_valor, 4)
   cat(sprintf("%s: ARI:%.4f NMI:%.4f ACC:%.4f \n",
               con_method, ari_valor, nmi_valor, acc_valor))
 }
@@ -163,11 +171,40 @@ sprintf("The %s method selected %d features.", method, length(top_features))
 B <- 10
 k <- 5
 
-run_RPClu_experiments(data=data$x[, top_features],
-                      ref_labels = as.integer(data$y),
-                      algorithms = algorithms,
-                      B=B,
-                      k=k)
+experiments <- run_RPClu_experiments(data=data$x,
+                                     top_features,
+                                     ref_labels = as.integer(data$y),
+                                     algorithms = algorithms,
+                                     B=B,
+                                     k=k)
+print(experiments)
+
+experiments_data_all <- load_experiments()
+
+filter_meat <- experiments_data_all$experiment_id >= experiments[["first"]] &
+  experiments_data_all$experiment_id <= experiments[["last"]]
+
+experiments_meat <- experiments_data_all[filter_meat,]
+
+# Extract ARI values
+ari_vals <- sapply(experiments_meat$external_metrics, function(x) x$ari)
+
+# Get indices of the top 10 ARI scores
+top10_idx <- order(ari_vals, decreasing = TRUE)[1:10]
+
+# Build the summary table
+top10_summary <- data.frame(
+  clustering_method = experiments_meat$clustering_method[top10_idx],
+  ensemble_method = experiments_meat$ensemble_method[top10_idx],
+  ari = round(ari_vals[top10_idx], 4)
+)
+
+print(top10_summary)
+
+
+
+
+
 
 
 run_experiment_individual_algorithm(data = data$x,
