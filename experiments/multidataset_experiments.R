b2f7fcbea0f27ee96481527d8b3418387f9a6dc9
antoniocarlosrodriguezbajo@gmail.com <antoniocarlosrodriguezbajo@gmail.comm>
2025-06-22 14:15:03 +0200

    More experiments in multidataset_experiments.

--- experiments/multidataset_experiments.R
+++ experiments/multidataset_experiments.R
@@ -19,9 +19,9 @@ calculate_consensus_labels <- function(E, k, ref_labels, method = NULL) {
   consensus_methods <- list(
     kmodes   = function(E) k_modes(E, is.relabelled = FALSE, seed = 1),
     majority = function(E) majority_voting(E, is.relabelled = FALSE),
-    lce      = function(E) LCE(E, k, sim.mat = "cts"),
-    lca      = function(E) LCA(E, is.relabelled = FALSE, seed = 1),
-    cspa     = function(E) CSPA(E_4d, k)
+    LCE      = function(E) LCE(E, k, sim.mat = "cts"),
+    LCA      = function(E) LCA(E, is.relabelled = FALSE, seed = 1),
+    CSPA     = function(E) CSPA(E_4d, k)
   )
 
   # If a specific method is provided, apply only that one
@@ -88,8 +88,8 @@ run_RPClu_experiments <- function(data_all,
                                   rp = TRUE,
                                   method_rp = "gaussian",
                                   seed = 101) {
-  data <- data_all[,top_features]
-  experiment_ids <- c()
+  data = data_all[,top_features]
+  experiment_ids = c()
   for (alg in algorithms) {
     print(alg)
     tryCatch({
@@ -147,7 +147,7 @@ run_RPClu_experiments <- function(data_all,
           external_metrics = list(ari = ari_valor, nmi = nmi_valor, acc = acc_valor)
         )
         save_experiment(exp_data)
-        experiment_ids <- c(experiment_ids, exp_data$experiment_id)
+        experiment_ids = c(experiment_ids, exp_data$experiment_id)
       }
 
     },
@@ -157,6 +157,47 @@ run_RPClu_experiments <- function(data_all,
   }
   return(list(first = experiment_ids[1], last = tail(experiment_ids, 1)))
 }
+
+
+run_clustering_experiments <- function(data_all,
+                                       top_features,
+                                       ref_labels,
+                                       algorithms,
+                                       k,
+                                       dataset = "Meat",
+                                       UFS_method = "Inf-FS2020",
+                                       consensus_method = NULL,
+                                       seed = 101) {
+  set.seed(seed)
+  experiment_ids = c()
+  data = data_all[,top_features]
+  results_dice = dice(data = data,
+                      nk = k,
+                      algorithms = algorithms,
+                      cons.funs = consensus_method,
+                      evaluate = TRUE,
+                      ref.cl = ref_labels,
+                      seed = seed,
+                      seed.data = seed)
+
+  clustering_lables = as.vector(results_dice$clusters[ , 2])
+  print(clustering_lables)
+  external_metrics = results_dice$indices$ei[["5"]][results_dice$indices$ei[["5"]]$Algorithms == best_ensemble_upper, -1]
+  rownames(external_metrics) <- NULL
+  ari_value = adjustedRandIndex(clustering_lables, ref_labels)
+  external_metrics_list = list(ari = ari_value,
+                               nmi = external_metrics$NMI,
+                               acc = external_metrics$accuracy)
+  internal_metrics = results_dice$indices$ii[["5"]][results_dice$indices$ii[["5"]]$Algorithms == best_ensemble_upper, -1]
+  rownames(internal_metrics) <- NULL
+
+  print(external_metrics_list)
+  print(internal_metrics)
+
+
+
+}
+
 
 summarize_top_metrics <- function(experiments, top_n = 10) {
   # Load all saved experiments
@@ -269,6 +310,7 @@ experiments <- run_RPClu_experiments(data_all=data$x,
                                      rp = TRUE,
                                      seed = seed)
 print(experiments)
+## 248-297##
 
 top_summary <- summarize_top_metrics(experiments, top_n = 10)
 top_ari <- subset(top_summary, metric == "ARI")
@@ -280,7 +322,7 @@ best_ensemble <- top_ari$ensemble_method[1]
 n_reps <- 20
 for (i in 1:n_reps) {
   seed <- 100 + i
-  experiments_rep <- run_RPClu_experiments(data = data$x,
+  experiments_rep <- run_RPClu_experiments(data_all = data$x,
                                        top_features = top_features,
                                        ref_labels = as.integer(data$y),
                                        algorithms = best_clustering,
@@ -295,16 +337,125 @@ for (i in 1:n_reps) {
 evaluate_experiments(experiments$last + 1,
                      experiments$last + 1 + n_reps - 1)
 
+# 298-317
+# Mean ARI: 0.6862 | SD ARI: 0.0061
+# Mean NMI: 0.8409 | SD NMI: 0.0162
+# Mean ACC: 0.7587 | SD ACC: 0.0268
 
+experiments$last <- experiments$last + 1 + n_reps - 1
 
 ####################
-# Inf-FS2020 (NO RP)
+# RP All features
 ####################
 
 n_reps <- 20
 for (i in 1:n_reps) {
   seed <- 100 + i
-  experiments_rep <- run_RPClu_experiments(data = data$x,
+  experiments_rep <- run_RPClu_experiments(data_all = data$x,
+                                           top_features = colnames(data$x),
+                                           ref_labels = as.integer(data$y),
+                                           algorithms = best_clustering,
+                                           method = best_ensemble,
+                                           B = B, k = k,
+                                           dataset = "Meat",
+                                           rp = TRUE,
+                                           seed = seed)
+  print(experiments_rep$first)
+}
+
+evaluate_experiments(experiments$last + 1,
+                     experiments$last + 1 + n_reps - 1)
+
+# 318-337
+# Mean ARI: 0.2996 | SD ARI: 0.0175
+# Mean NMI: 0.5239 | SD NMI: 0.0279
+# Mean ACC: 0.5247 | SD ACC: 0.0271
+
+
+experiments$last <- experiments$last + 1 + n_reps - 1
+
+
+####################
+# GMM All features
+####################
+
+n_reps <- 20
+for (i in 1:n_reps) {
+  seed <- 100 + i
+  experiments_rep <- run_clustering_experiments(data_all = data$x,
+                                           top_features = colnames(data$x),
+                                           ref_labels = as.integer(data$y),
+                                           algorithms = best_clustering,
+                                           consensus_method = best_ensemble,
+                                           k = k,
+                                           dataset = "Meat",
+                                           UFS_method = "No UFS",
+                                           seed = seed)
+  print(experiments_rep$first)
+}
+
+
+
+
+
+
+seed <- 101
+n_reps <- 20
+best_ensemble_upper = toupper(best_ensemble)
+for (i in 1:n_reps) {
+  seed = 100 + i
+  results_dice = dice(data = data$x,
+                       nk = k,
+                       algorithms = best_clustering,
+                       cons.funs = best_ensemble_upper,
+                       evaluate = TRUE,
+                       ref.cl = as.integer(data$y),
+                       seed = seed,
+                       seed.data = seed
+                       )
+  clustering_lables = as.vector(results_dice$clusters[ , 2])
+  # print(clustering_lables)
+  external_metrics = results_dice$indices$ei[["5"]][results_dice$indices$ei[["5"]]$Algorithms == best_ensemble_upper, -1]
+  ari = adjustedRandIndex(clustering_lables, as.integer(data$y))
+  internal_metrics = results_dice$indices$ii[["5"]][results_dice$indices$ii[["5"]]$Algorithms == best_ensemble_upper, -1]
+  rownames(internal_metrics) <- NULL
+  print(seed)
+  print(ari)
+  print(external_metrics)
+  print(internal_metrics)
+}
+
+
+
+results_dice2 <- dice(data = data$x,
+                     nk = k,
+                     algorithms = best_clustering,
+                     cons.funs = "LCE",
+                     evaluate = TRUE,
+                     ref.cl = as.integer(data$y),
+                     seed = seed +1 ,
+                     seed.data = seed +1
+)
+
+run_gmm_experiments(data_all = data$x,
+                    top_features = top_features,
+                    ref_labels = as.integer(data$y),
+                    k = 5,
+                    reps = 20,
+                    seed = 101)
+
+run_clustering_experiments(data_all = data$x,
+                           top_features = top_features,
+                           ref_labels = as.integer(data$y),
+                           cluster_algorithm = "km",
+                           k = 4)
+
+
+
+n_reps <- 20
+for (i in 1:n_reps) {
+  seed <- 100 + i
+  experiments_rep <- run_RPClu_experiments(data_all = data$x,
                                            top_features = top_features,
                                            ref_labels = as.integer(data$y),
                                            algorithms = "km",
@@ -316,10 +467,78 @@ for (i in 1:n_reps) {
   print(experiments_rep$first)
 }
 
-set.seed(2333)
-cc <- consensus_cluster(data$x[,top_features], nk = 4, reps = 1, p.item = 1, algorithms = "km", progress = FALSE)
+set.seed(101)
+cc <- consensus_cluster(data$x[,top_features], nk = 4, reps = 20, p.item = 1, algorithms = "gmm", progress = FALSE)
 ari <- adjustedRandIndex(cc, as.integer(data$y))
 cat("ARI:", round(ari, 7), "\n")
+
+
+# Obtener los nombres de las columnas (R1, R2, ..., R20)
+r_labels <- dimnames(cc)[[2]]
+
+# Recorrer cada etiqueta y extraer los valores
+for (etiqueta in r_labels) {
+  clustering_lables <- as.vector(cc[, etiqueta, 1, 1])
+
+  # Puedes hacer lo que quieras con "valores", por ejemplo imprimir:
+  print(clustering_lables)
+
+
+}
+
+
+library(mclust)
+set.seed(1)
+data <- matrix(rnorm(200), ncol = 2)
+for (i in 1:5) {
+  set.seed(100 + i)
+  randPairs <- hcRandomPairs(data)
+  mod <- Mclust(data, G = 2, initialization = list(hcPairs = randPairs), verbose = FALSE)
+  print(table(mod$classification))
+}
+
+
+set.seed(123) # Semilla 1
+mod1 <- Mclust(data$x, G = k)
+clustering1 <- as.vector(mod1$classification)
+
+set.seed(4506) # Semilla 2, genera otro clustering
+mod2 <- Mclust(data$x, G = k)
+clustering2 <- as.vector(mod2$classification)
+
+library(mclust)
+
+# Número de repeticiones
+n_reps <- 20
+# Semillas distintas para reproducibilidad
+semillas <- sample(1:10000, n_reps)
+
+# Supón que tienes las etiquetas verdaderas en 'clase_real'
+# clase_real <- ...
+
+# Vector para guardar la métrica (por ejemplo, Rand Index ajustado)
+metricas <- numeric(n_reps)
+
+for(i in seq_len(n_reps)) {
+  set.seed(semillas[i])
+  # Opcional: usar inicialización explícitamente aleatoria
+  hc_init <- hcRandomPairs(data$x, seed = semillas[i])
+  mod <- Mclust(data$x, initialization = list(hcPairs = hc_init))
+
+  # O simplemente:
+  # mod <- Mclust(data$x)
+
+  clustering <- mod$classification
+
+  # Ejemplo de métrica: Rand Index ajustado (si tienes etiquetas verdaderas)
+  metricas[i] <- adjustedRandIndex(clustering, as.numeric(data$y))
+}
+
+media_metrica <- mean(metricas)
+sd_metrica <- sd(metricas)
+
+cat("Media de la métrica externa:", media_metrica, "\n")
+cat("Desviación estándar:", sd_metrica, "\n")
 
 ############################
 # ALLAML  TBC
